19555a760b17c7e11a9356aafee4e92d1831b6fe
Jia <wngjia2013@gmail.com>
2020-01-21 10:04:18 -0600

    lec03: better hello world

--- src/main/java/ece448/lec03/HelloWorld.java
+++ src/main/java/ece448/lec03/HelloWorld.java
@@ -1,7 +1,13 @@
 package ece448.lec03;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		logger.info("Hello World");
 	}
+
+	private static final Logger logger
+		= LoggerFactory.getLogger(HelloWorld.class);
 }
