a01baf5c886f25646b24516c908f910560f20550
ubuntu <jwang@ece>
2020-01-28 13:12:16 -0600

    lec06: multithreading

--- src/main/java/ece448/lec06/ReverseServer.java
+++ src/main/java/ece448/lec06/ReverseServer.java
@@ -17,10 +17,13 @@ public class ReverseServer implements AutoCloseable {
     public void run() throws Exception {
         logger.info("server: port {}", server.getLocalPort());
         for (;;) {
+            // wait for client to connect
             Socket client = server.accept();
-            logger.info("client: address {}",
-                client.getRemoteSocketAddress());
-            client.close();
+            logger.info("client {}: connected", client.getRemoteSocketAddress());
+            // start a thread to process client request
+            Thread t = new Thread(new ReverseProcessor(client));
+            t.setDaemon(true);
+            t.start();
         }
     }
 
